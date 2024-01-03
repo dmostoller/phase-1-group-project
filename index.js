40b087a3a7041690bf9b5819f5ae01cb94a34d3e
David Mostoller <dmostoller@gmail.com>
2024-01-03 16:02:20 -0500

    more styling

--- index.js
+++ index.js
@@ -5,38 +5,35 @@ const apiKey = "93f5cc80-f6d0-4f03-a4c7-fce468ed501d"
 //    .then((data) => console.log(data))
 fetch('http://localhost:3000/featured-events')
 .then((resp) => resp.json())
-.then((featuredEventsArray) => renderFeatured(featuredEventsArray))
+.then((data) => renderFeatured(data))
 
 function renderFeatured(featuredEventsArray) {
 //featuredEventsArray is an array of objects
 // designate where they will go
 const featuredContainer = document.querySelector("#featured-events")
-
 featuredEventsArray.forEach((featuredObj) => {
-const featuredDiv = document.createElement('div')
-featuredDiv.className = "featured-card"
-featuredContainer.appendChild(featuredDiv)
-const featuredName = document.createElement('h3')
-const featuredImg = document.createElement('img')
-const featuredDate = document.createElement('p')
-const featuredVenue = document.createElement('p')
-const bookmark = document.createElement('img')
+    const featuredDiv = document.createElement('div')
+    featuredDiv.className = "featured-card"
+    featuredContainer.appendChild(featuredDiv)
+    const featuredName = document.createElement('h3')
+    const featuredImg = document.createElement('img')
+    featuredImg.className = 'featured-img'
+    const featuredDate = document.createElement('p')
+    const featuredVenue = document.createElement('p')
+    const bookmark = document.createElement('img')
 
+    featuredName.textContent = featuredObj.name
+    featuredImg.src = featuredObj.image
+    featuredDate.textContent = featuredObj.date
+    featuredVenue.textContent = featuredObj.venue
+    bookmark.className = 'bookmark'
+    bookmark.src = "./assets/bookmark_empty.png"
 
-featuredName.textContent = featuredObj.name
-featuredImg.src = featuredObj.image
-featuredDate.textContent = featuredObj.date
-featuredVenue.textContent = featuredObj.venue
-bookmark.className = 'bookmark'
-bookmark.src = "./assets/bookmark_empty.png"
-
-featuredDiv.appendChild(featuredImg)
-featuredDiv.appendChild(featuredName)
-featuredDiv.appendChild(featuredVenue)
-featuredDiv.appendChild(featuredDate)
-featuredDiv.appendChild(bookmark)
-
-
+    featuredDiv.appendChild(featuredImg)
+    featuredDiv.appendChild(featuredName)
+    featuredDiv.appendChild(featuredVenue)
+    featuredDiv.appendChild(featuredDate)
+    featuredDiv.appendChild(bookmark)
 })
 
 }
@@ -80,9 +77,19 @@ function fetchStateEvents(stateId, apiKey) {
         saveEvent.appendChild(bookmark)
         eventDetailsDiv.appendChild(saveEvent)
         
-        
-        //create eventlistener and function to change bookmark color on mouseover
+
+         //create eventlistener and function to change bookmark color on mouseover
+        //eventCard.addEventListener('mouseover', () => {
+            //bookmark.src = "./assets/bookmark_full.png";
+        //});
         //create eventlistener and function to change bookmark color on click
+        eventCard.addEventListener('click', () => {
+            if (bookmark.src.includes("./assets/bookmark_empty.png")) {
+                bookmark.src = "./assets/bookmark_full.png";
+            } else {
+                bookmark.src = "./assets/bookmark_empty.png";
+            }
+        });
         eventsList.appendChild(eventCard)
         //make it so that the previous city selection gets cleared before creating new list
         })
@@ -98,6 +105,7 @@ function displayEvents(e) {
     document.querySelectorAll('.event-card').forEach(e => e.remove());
     fetchStateEvents(stateId, apiKey)
     hideBanner()
+    hideFeatured()
 }
 
 
@@ -105,8 +113,17 @@ function displayEvents(e) {
 function hideBanner() {
     const bannerDiv = document.querySelector('#concert-banner')
         if (bannerDiv.style.display === "none") {
-            bannerDiv.style.display = "blocl";
+            bannerDiv.style.display = "block";
         } else {
             bannerDiv.style.display = "none";
         }
     } 
+function hideFeatured() {
+    const featured = document.querySelector('#featured-events')
+        if (featured.style.display === "none") {
+            featured.style.display = "block";
+        } else {
+            featured.style.display = "none";
+        }
+
+}
