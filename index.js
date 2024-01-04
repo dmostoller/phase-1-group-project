b9e8bf5d2012c4fb1685745723ff5f21b13e09e3
David Mostoller <dmostoller@gmail.com>
2024-01-04 15:15:32 -0500

    added saved events view, ability to save events from api to json database, functions to show/hide various divs

--- index.js
+++ index.js
@@ -1,64 +1,69 @@
 //const apiKey = process.env.API_KEY
 const apiKey = "93f5cc80-f6d0-4f03-a4c7-fce468ed501d"
-//fetch("https://www.jambase.com/jb-api/v1/events?apikey=93f5cc80-f6d0-4f03-a4c7-fce468ed501d")
-//    .then((resp) => resp.json())
-//    .then((data) => console.log(data))
-fetch('http://localhost:3000/featured-events')
+
+//render featured events section
+fetch('http://localhost:3000/saved-events')
 .then((resp) => resp.json())
 .then((data) => renderFeatured(data))
 
 function renderFeatured(featuredEventsArray) {
-//featuredEventsArray is an array of objects
-// designate where they will go
 const featuredContainer = document.querySelector("#featured-events")
 featuredEventsArray.forEach((featuredObj) => {
-    const featuredDiv = document.createElement('div')
-    featuredDiv.className = "featured-card"
-    featuredContainer.appendChild(featuredDiv)
-    const featuredName = document.createElement('h3')
-    const featuredImg = document.createElement('img')
-    featuredImg.className = 'featured-img'
-    const featuredDate = document.createElement('p')
-    const featuredVenue = document.createElement('p')
-    const bookmark = document.createElement('img')
+        //console.log(featuredObj)
+        const featuredDiv = document.createElement('div')
+        featuredDiv.className = "featured-card"
+        const featuredName = document.createElement('h3')
+        const featuredImg = document.createElement('img')
+        featuredImg.className = 'featured-img'
+        const featuredDate = document.createElement('p')
+        const featuredVenue = document.createElement('p')
+        const bookmark = document.createElement('img')
+        //add 'a' tag to name to make it a link, set to info from db.json, append to dom
+        
+        //add 'a'tag to venue name to make it a link, set to info from db.json, append to dom
 
-    featuredName.textContent = featuredObj.name
-    featuredImg.src = featuredObj.image
-    featuredDate.textContent = featuredObj.date
-    featuredVenue.textContent = featuredObj.venue
-    bookmark.className = 'bookmark'
-    bookmark.src = "./assets/bookmark_empty.png"
-
-    featuredDiv.appendChild(featuredImg)
-    featuredDiv.appendChild(featuredName)
-    featuredDiv.appendChild(featuredVenue)
-    featuredDiv.appendChild(featuredDate)
-    featuredDiv.appendChild(bookmark)
-
-    // bookmark.addEventListener('mouseover', () => {
-    //     bookmark.src = "./assets/bookmark_full.png";
-    // });
-    // bookmark.addEventListener('mouseout', () => {
-    //     bookmark.src = "./assets/bookmark_empty.png";
-    // });
-    bookmark.addEventListener('click', () => {
-        if (bookmark.src.includes("./assets/bookmark_full.png")) {
-            bookmark.src = "./assets/bookmark_empty.png";
-        } else {
-            bookmark.src = "./assets/bookmark_full.png";
+        featuredName.textContent = featuredObj.name
+        featuredImg.src = featuredObj.image
+        featuredDate.textContent = featuredObj.date
+        featuredVenue.textContent = featuredObj.venue
+        
+        //stop forEach loop after 3 entries
+        if (featuredObj.id > 3){
+            return;
         }
-    });
-})
+        featuredContainer.appendChild(featuredDiv)
+        featuredDiv.appendChild(featuredImg)
+        featuredDiv.appendChild(featuredName)
+        featuredDiv.appendChild(featuredVenue)
+        featuredDiv.appendChild(featuredDate)
 
+
+    })
+}
+//show state view on dropdown menu change
+const dropDown = document.querySelector('#state-list')
+dropDown.addEventListener('change', (e) => displayEvents(e))
+function displayEvents(e) {
+    e.preventDefault()
+    const stateId = e.target.value
+    //console.log(stateId)
+    document.querySelectorAll('.event-card').forEach(e => e.remove());
+    fetchStateEvents(stateId, apiKey)
+    hideBanner()
+    hideFeatured()
+    hideSavedEvents()
+    showHome()
+    showStateView()
 }
 
+//render list of events by state view
 function fetchStateEvents(stateId, apiKey) {
     fetch(`https://www.jambase.com/jb-api/v1/events?geoStateIso=${stateId}&apikey=${apiKey}`)
     .then((resp) => resp.json())
     .then((data) => renderEventList(data))
 
     function renderEventList(eventArray){
-        console.log(eventArray)
+        //console.log(eventArray)
         const eventsList = document.querySelector("#event-container")
         eventArray.events.forEach((eventObj) => {
             const eventCard = document.createElement('div')
@@ -74,7 +79,6 @@ function fetchStateEvents(stateId, apiKey) {
             eventCard.appendChild(eventDetailsDiv)
             eventDetailsDiv.className = 'event-details'
             const title = document.createElement('h3') 
-            //title.textContent = eventObj.name
             const titleUrl = document.createElement('a')
             titleUrl.href = eventObj.url
             titleUrl.target = "_blank"
@@ -91,48 +95,116 @@ function fetchStateEvents(stateId, apiKey) {
             venueUrl.textContent = eventObj.location.name
             venue.appendChild(venueUrl)
             eventDetailsDiv.appendChild(venue)
-            //create and append the bookmark graphic 
             const saveEvent = document.createElement('div')
             saveEvent.className = 'save-event'
             const bookmark = document.createElement('img')
             bookmark.src = "./assets/bookmark_empty.png"
             bookmark.className = 'bookmark'
             saveEvent.appendChild(bookmark)
-            eventDetailsDiv.appendChild(saveEvent)
-            
-            //create eventlistener and function to change bookmark color on mouseover
-            // bookmark.addEventListener('mouseover', () => {
-            //     bookmark.src = "./assets/bookmark_full.png";
-            // });
-            // bookmark.addEventListener('mouseout', () => {
-            //     bookmark.src = "./assets/bookmark_empty.png";
-            // });
-            //create eventlistener and function to change bookmark color on click
-            
-            bookmark.addEventListener('click', () => {
-                if (bookmark.src.includes("./assets/bookmark_full.png")) {
-                    bookmark.src = "./assets/bookmark_empty.png";
-                } else {
-                    bookmark.src = "./assets/bookmark_full.png";
+            eventDetailsDiv.appendChild(saveEvent) 
+            eventsList.appendChild(eventCard)   
+    //bookmark change color 
+            bookmark.addEventListener('mouseover', function() {
+                bookmark.src= "./assets/bookmark_full.png"
+                })
+            bookmark.addEventListener('mouseout', function() {
+                bookmark.src= "./assets/bookmark_empty.png"
+                })
+    //save event on bookmark click
+            bookmark.addEventListener('click', (e) => handleSaveEvent(e))
+            function handleSaveEvent(e) {
+                e.preventDefault()
+                let newSavedEvent = {
+                    name: eventObj.name,
+                    date: eventObj.startDate,
+                    url: eventObj.url,
+                    image: eventObj.image,
+                    venue: eventObj.location.name,
+                    venueurl: eventObj.location.url,
                 }
-            });
-            eventsList.appendChild(eventCard)
+                fetch('http://localhost:3000/saved-events', {
+                    method : 'POST',
+                    headers : {
+                        'Accept' : 'application/json',
+                        'Content-Type' : 'application/json'
+                    },
+                    body : JSON.stringify(newSavedEvent)
+                })
+                renderSavedEvents()
+            }   
         })
     }    
 }
 
-const form = document.querySelector('#search-form')
-form.addEventListener('submit', (e) => displayEvents(e))
-function displayEvents(e) {
-    e.preventDefault()
-    const stateId = e.target['state-list'].value
-    //console.log(stateId)
-    document.querySelectorAll('.event-card').forEach(e => e.remove());
-    fetchStateEvents(stateId, apiKey)
-    hideBanner()
-    hideFeatured()
-}
 
+
+//function for loading saved events view from db.json
+//function renderSavedEvents
+    function renderSavedEvents() {
+        fetch('http://localhost:3000/saved-events')
+        .then((resp) => resp.json())
+        .then((data) => renderSaved(data))
+    }
+    function renderSaved(savedEventsArray) {
+        const savedEventsList = document.querySelector("#saved-events")
+        savedEventsArray.forEach((savedEventObj) => {
+            //console.log(savedEventObj)
+            const eventCard = document.createElement('div')
+            eventCard.className = 'event-card'
+            const imageThumbDiv = document.createElement('div')
+            eventCard.appendChild(imageThumbDiv)
+            const img = document.createElement('img')
+            img.src = savedEventObj.image
+            img.className = 'image'
+            imageThumbDiv.className = 'event-thumb'
+            imageThumbDiv.appendChild(img)
+            const eventDetailsDiv = document.createElement('div')
+            eventCard.appendChild(eventDetailsDiv)
+            eventDetailsDiv.className = 'event-details'
+            const title = document.createElement('h3') 
+            const titleUrl = document.createElement('a')
+            titleUrl.href = savedEventObj.url
+            titleUrl.target = "_blank"
+            titleUrl.textContent = savedEventObj.name
+            title.appendChild(titleUrl)
+            eventDetailsDiv.appendChild(title)
+            const date = document.createElement('p')
+            date.textContent = savedEventObj.date
+            eventDetailsDiv.appendChild(date)
+            const venue = document.createElement('p')
+            const venueUrl = document.createElement('a')
+            venueUrl.target = "_blank"
+            venueUrl.href = savedEventObj.venueurl
+            venueUrl.textContent = savedEventObj.venue
+            venue.appendChild(venueUrl)
+            eventDetailsDiv.appendChild(venue)
+            const deleteEvent = document.createElement('div')
+            deleteEvent.className = 'delete-event'
+            const trashCan = document.createElement('img')
+            trashCan.src = "./assets/trash-can-white.png"
+            trashCan.className = 'bookmark'
+            deleteEvent.appendChild(trashCan)
+            eventDetailsDiv.appendChild(deleteEvent) 
+            savedEventsList.appendChild(eventCard)
+
+            //change color of trashcan
+            trashCan.addEventListener('mouseover', function() {
+                trashCan.src= "./assets/trash-can-grey.png"
+            })
+            trashCan.addEventListener('mouseout', function() {
+                trashCan.src= "./assets/trash-can-white.png"
+            })
+//////////////add eventlistener for click with callback function to delete the saved event
+
+//////////////create function that does a fetch delete to remove the saved event from db.json
+
+        })
+            hideStateView()
+            hideFeatured()
+            hideBanner()
+            showHome()
+            showSavedEvents()
+    }
 
 // function for hiding and showing banner
 function hideBanner() {
@@ -142,12 +214,57 @@ function hideBanner() {
 function hideFeatured() {
     const featured = document.querySelector('#featured-events')
     featured.style.display = "none"
+    }
+function hideStateView() {
+    const stateView = document.querySelector('#event-container')
+    stateView.style.display = "none"
+    }
+function showStateView() {
+    const stateView = document.querySelector('#event-container')
+    stateView.style.display = "block"
+    }
+function hideSavedEvents() {
+    const savedEventsView = document.querySelector('#saved-events')
+    savedEventsView.style.display = "none" 
+}    
+function showSavedEvents() {
+    const savedEventsView = document.querySelector('#saved-events')
+    savedEventsView.style.display = "block" 
 }
-
+//change banner image on mouseover
 const bannerImage = document.querySelector('.banner-image')
 bannerImage.addEventListener('mouseover', function() {
     bannerImage.src = "./assets/concert-2.jpg"
-})
+    })
 bannerImage.addEventListener('mouseout', function() {
     bannerImage.src = "./assets/concert-1.jpg"
-})
+    })
+//change home icon states
+const homeIcon = document.querySelector('#home-icon')
+function showHome() {
+    homeIcon.style.display = "block"
+    }
+homeIcon.addEventListener('mouseover', function() {
+    homeIcon.src= "./assets/home-hover.png"
+    })
+homeIcon.addEventListener('mouseout', function() {
+    homeIcon.src= "./assets/home.png"
+    })
+//change saved icon states
+const savedIcon = document.querySelector('#saved-icon')
+savedIcon.addEventListener('mouseover', function() {
+    savedIcon.src= "./assets/bookmark-small-grey.png"
+    })
+savedIcon.addEventListener('mouseout', function() {
+    savedIcon.src= "./assets/bookmark-small-blk.png"
+    })
+//navigate to saved events page on click
+savedIcon.addEventListener('click', (e) => renderSavedEvents(e))
+const trashIcon = document.querySelector('#trash-icon')
+    trashIcon.addEventListener('mouseover', function() {
+    trashIcon.src= "./assets/trash-can-grey.png"
+    })
+    trashIcon.addEventListener('mouseout', function() {
+        trashIcon.src= "./assets/trash-can-white.png"
+    })
+
