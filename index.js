068acc0e593cc11ef408ca372e96fa5e06efda74
David Mostoller <dmostoller@gmail.com>
2024-01-05 10:59:56 -0500

    cleaned up repetitive code like the show and hide div functions - combined all into one by passing in the id as a variable

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-//const apiKey = process.env.API_KEY
 const apiKey = "93f5cc80-f6d0-4f03-a4c7-fce468ed501d"
+// const path = require('path')
+//require('dotenv').config()
+// import 'dotenv/config'
+//console.log(process.env.API_KEY)
+//const apiKey = process.env.API_KEY
+
+
+
 
 //render featured events section
 fetch('http://localhost:3000/saved-events')
@@ -50,11 +57,11 @@ function displayEvents(e) {
     //console.log(stateId)
     document.querySelectorAll('.event-card').forEach(e => e.remove());
     fetchStateEvents(stateId, apiKey)
-    hideBanner()
-    hideFeatured()
-    hideSavedEvents()
-    showHome()
-    showStateView()
+    hideDiv("#concert-banner")
+    hideDiv("#featured-events")
+    showDiv("#event-container")
+    hideDiv("#saved-events")
+    showDiv("#home-icon")
 }
 
 //render list of events by state view
@@ -68,53 +75,52 @@ function fetchStateEvents(stateId, apiKey) {
         const eventsList = document.querySelector("#event-container")
         eventArray.events.forEach((eventObj) => {
             const eventCard = document.createElement('div')
-            eventCard.className = 'event-card'
+                eventCard.className = 'event-card'
             const imageThumbDiv = document.createElement('div')
             eventCard.appendChild(imageThumbDiv)
             const img = document.createElement('img')
-            img.src = eventObj.image
-            img.className = 'image'
-            imageThumbDiv.className = 'event-thumb'
-            imageThumbDiv.appendChild(img)
+                img.src = eventObj.image
+                img.className = 'image'
+                imageThumbDiv.className = 'event-thumb'
+                imageThumbDiv.appendChild(img)
             const eventDetailsDiv = document.createElement('div')
             eventCard.appendChild(eventDetailsDiv)
-            eventDetailsDiv.className = 'event-details'
+                eventDetailsDiv.className = 'event-details'
             const title = document.createElement('h3') 
             const titleUrl = document.createElement('a')
-            titleUrl.href = eventObj.url
-            titleUrl.target = "_blank"
-            titleUrl.textContent = eventObj.name
-            title.appendChild(titleUrl)
+                titleUrl.href = eventObj.url
+                titleUrl.target = "_blank"
+                titleUrl.textContent = eventObj.name
+                title.appendChild(titleUrl)
             eventDetailsDiv.appendChild(title)
             const date = document.createElement('p')
-            date.textContent = eventObj.startDate
+                date.textContent = eventObj.startDate
             eventDetailsDiv.appendChild(date)
             const venue = document.createElement('p')
             const venueUrl = document.createElement('a')
-            venueUrl.target = "_blank"
-            venueUrl.href = eventObj.location.url
-            venueUrl.textContent = eventObj.location.name
+                venueUrl.target = "_blank"
+                venueUrl.href = eventObj.location.url
+                venueUrl.textContent = eventObj.location.name
             venue.appendChild(venueUrl)
             eventDetailsDiv.appendChild(venue)
             const saveEvent = document.createElement('div')
-            saveEvent.className = 'save-event'
+                saveEvent.className = 'save-event'
             const bookmark = document.createElement('img')
-            bookmark.src = "./assets/bookmark_empty.png"
-            bookmark.className = 'bookmark'
+                bookmark.src = "./assets/bookmark_empty.png"
+                bookmark.className = 'bookmark'
             saveEvent.appendChild(bookmark)
             eventDetailsDiv.appendChild(saveEvent) 
             eventsList.appendChild(eventCard)   
-    //bookmark change color 
-            bookmark.addEventListener('mouseover', function() {
-                bookmark.src= "./assets/bookmark_full.png"
-                })
-            bookmark.addEventListener('mouseout', function() {
-                bookmark.src= "./assets/bookmark_empty.png"
-                })
-    //save event on bookmark click
+
+            bookmark.addEventListener('click', () => {
+                if (bookmark.src == "./assets/bookmark_full.png") {
+                    bookmark.src = "./assets/bookmark_empty.png";
+                } else {
+                    bookmark.src = "./assets/bookmark_full.png";
+                }
+    });  
             bookmark.addEventListener('click', (e) => handleSaveEvent(e))
             function handleSaveEvent(e) {
-                e.preventDefault()
                 let newSavedEvent = {
                     name: eventObj.name,
                     date: eventObj.startDate,
@@ -131,13 +137,10 @@ function fetchStateEvents(stateId, apiKey) {
                     },
                     body : JSON.stringify(newSavedEvent)
                 })
-                renderSavedEvents()
             }   
         })
     }    
 }
-
-
 
 //function for loading saved events view from db.json
 //function renderSavedEvents
@@ -152,39 +155,39 @@ function fetchStateEvents(stateId, apiKey) {
         savedEventsArray.forEach((savedEventObj) => {
             //console.log(savedEventObj)
             const eventCard = document.createElement('div')
-            eventCard.className = 'saved-card'
+                eventCard.className = 'saved-card'
             const imageThumbDiv = document.createElement('div')
             eventCard.appendChild(imageThumbDiv)
             const img = document.createElement('img')
-            img.src = savedEventObj.image
-            img.className = 'image'
-            imageThumbDiv.className = 'event-thumb'
+                img.src = savedEventObj.image
+                img.className = 'image'
+                imageThumbDiv.className = 'event-thumb'
             imageThumbDiv.appendChild(img)
             const eventDetailsDiv = document.createElement('div')
             eventCard.appendChild(eventDetailsDiv)
-            eventDetailsDiv.className = 'event-details'
+                eventDetailsDiv.className = 'event-details'
             const title = document.createElement('h3') 
             const titleUrl = document.createElement('a')
-            titleUrl.href = savedEventObj.url
-            titleUrl.target = "_blank"
-            titleUrl.textContent = savedEventObj.name
-            title.appendChild(titleUrl)
+                titleUrl.href = savedEventObj.url
+                titleUrl.target = "_blank"
+                titleUrl.textContent = savedEventObj.name
+                title.appendChild(titleUrl)
             eventDetailsDiv.appendChild(title)
             const date = document.createElement('p')
-            date.textContent = savedEventObj.date
+                date.textContent = savedEventObj.date
             eventDetailsDiv.appendChild(date)
             const venue = document.createElement('p')
             const venueUrl = document.createElement('a')
-            venueUrl.target = "_blank"
-            venueUrl.href = savedEventObj.venueurl
-            venueUrl.textContent = savedEventObj.venue
+                venueUrl.target = "_blank"
+                venueUrl.href = savedEventObj.venueurl
+                venueUrl.textContent = savedEventObj.venue
             venue.appendChild(venueUrl)
             eventDetailsDiv.appendChild(venue)
             const deleteEvent = document.createElement('div')
-            deleteEvent.className = 'delete-event'
+                deleteEvent.className = 'delete-event'
             const trashCan = document.createElement('img')
-            trashCan.src = "./assets/trash-can-white.png"
-            trashCan.className = 'bookmark'
+                trashCan.src = "./assets/trash-can-white.png"
+                trashCan.className = 'bookmark'
             deleteEvent.appendChild(trashCan)
             eventDetailsDiv.appendChild(deleteEvent) 
             savedEventsList.appendChild(eventCard)
@@ -206,65 +209,48 @@ function fetchStateEvents(stateId, apiKey) {
                 savedEventsList.removeChild(eventCard)
             }
         })
-            hideStateView()
-            hideFeatured()
-            hideBanner()
-            showHome()
-            showSavedEvents()
+            hideDiv("#concert-banner")
+            hideDiv("#featured-events")
+            hideDiv("#event-container")
+            showDiv("#saved-events")
+            showDiv("#home-icon")
+
     }
 
-// function for hiding and showing banner
-function hideBanner() {
-    const bannerDiv = document.querySelector('#concert-banner')
-    bannerDiv.style.display = "none"
-    } 
-function hideFeatured() {
-    const featured = document.querySelector('#featured-events')
-    featured.style.display = "none"
-    }
-function hideStateView() {
-    const stateView = document.querySelector('#event-container')
-    stateView.style.display = "none"
-    }
-function showStateView() {
-    const stateView = document.querySelector('#event-container')
-    stateView.style.display = "block"
-    }
-function hideSavedEvents() {
-    const savedEventsView = document.querySelector('#saved-events')
-    savedEventsView.style.display = "none" 
-}    
-function showSavedEvents() {
-    const savedEventsView = document.querySelector('#saved-events')
-    savedEventsView.style.display = "block" 
+// functions for hiding and showing divs
+function hideDiv (divId) {
+    let divToHide = document.querySelector(`${divId}`)
+    divToHide.style.display = "none"
 }
-//change banner image on mouseover
+function showDiv (divId) {
+    let divToShow = document.querySelector(`${divId}`)
+    divToShow.style.display = "block"
+}
+
+//change images and icons on mouseover    
 const bannerImage = document.querySelector('.banner-image')
+const homeIcon = document.querySelector('#home-icon')
+const savedIcon = document.querySelector('#saved-icon')
+
 bannerImage.addEventListener('mouseover', function() {
     bannerImage.src = "./assets/concert-2.jpg"
     })
 bannerImage.addEventListener('mouseout', function() {
     bannerImage.src = "./assets/concert-1.jpg"
     })
-//change home icon states
-const homeIcon = document.querySelector('#home-icon')
-function showHome() {
-    homeIcon.style.display = "block"
-    }
 homeIcon.addEventListener('mouseover', function() {
     homeIcon.src= "./assets/home-hover.png"
     })
 homeIcon.addEventListener('mouseout', function() {
     homeIcon.src= "./assets/home.png"
     })
-//change saved icon states
-const savedIcon = document.querySelector('#saved-icon')
 savedIcon.addEventListener('mouseover', function() {
     savedIcon.src= "./assets/bookmark-small-grey.png"
     })
 savedIcon.addEventListener('mouseout', function() {
     savedIcon.src= "./assets/bookmark-small-blk.png"
     })
+
 //navigate to saved events page on click
 savedIcon.addEventListener('click', (e) => renderSavedEvents(e))
 
